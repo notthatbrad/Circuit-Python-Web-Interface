6c2a96a72b19a349c964117350b26c259ded0e09
notthatbrad <tbradt@gmail.com>
2017-09-26 12:18:31 -0400

    Adding initial code
    The first update adding the intial code

---
+++ main.py
@@ -0,0 +1,35 @@
+from machine import Pin
+
+#initialize data to defaults
+startD = "|~~|"
+endD= "~||~"
+ledPin = Pin(4, Pin.OUT)    # create output pin on GPIO0
+
+#function for toggling the pin on and off
+def toggle():
+	ledPin.value(not ledPin.value())
+	print('%sToggled%s' % (startD, endD))
+
+#function for turning on the pin
+def turnOn():
+	ledPin.on() 
+	print('%sTurned On%s' % (startD, endD))
+
+#function for turning off the pin
+def turnOff():
+	ledPin.off() 
+	print('%sTurned Off%s' % (startD, endD))
+
+	
+#to set custom delimiters. If not called, the default values will be used
+def setDelimiters(dStart, dEnd):
+	global startD
+	startD = dStart
+	global endD
+	endD = dEnd
+	print("%sConnected and ready for input.%s" % (startD, endD))
+	
+def changePinNumber(pNumber):
+	global ledPin
+	ledPin = Pin(pNumber, Pin.OUT)
+	print('%snow using pin %s %s, % (startD, pNumber, endD)
